a070c4ea372328c9647a71d35be8b63b1c2a29da
zi <z4none@gmail.com>
2022-08-09 23:28:04 +0800

    fix lint error

--- components/Comments.js
+++ components/Comments.js
@@ -1,32 +1,31 @@
-// import BLOG from '@/blog.config'
-import dynamic from 'next/dynamic'
-import { useRouter } from 'next/router'
-import { init } from '@waline/client';
-import '@waline/client/dist/waline.css';
+import BLOG from '@/blog.config'
+import '@waline/client/dist/waline.css'
 
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from 'react'
 
 export default class Comment extends PureComponent {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this._commentRef = React.createRef()
-   }
-  async componentDidMount() {
-    if (typeof window === "undefined") {
+  }
+
+  async componentDidMount () {
+    if (typeof window === 'undefined') {
       return
     }
-    if(!this._commentRef.current) {
+    if (!this._commentRef.current) {
       return
     }
-    const Waline= await import('@waline/client')
+    const Waline = await import('@waline/client')
     this.Waline = Waline.init({
-      el: this._commentRef.current, 
-      serverURL: 'https://cmt.z4none.me/',
+      el: this._commentRef.current,
+      serverURL: BLOG.waline,
       visitor: true,
-      path: this.props.slug, 
-     })
-   }
-  render() {
-        return <div id='comment' ref={this._commentRef} />
-   }
+      path: this.props.slug
+    })
+  }
+
+  render () {
+    return <div id='comment' ref={this._commentRef} />
+  }
 }
